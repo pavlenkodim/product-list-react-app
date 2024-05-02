78af924df41327d66f52dab751e50ea78065be40
pavlenkodim <pavlenkodim@mail.ru>
2024-05-02 11:46:42 +0500

    Form is done! Have small problems to input -> localstorage

--- src/components/add/Add.jsx
+++ src/components/add/Add.jsx
@@ -11,40 +11,75 @@ const Add = () => {
     } = useForm()
     
     const onSubmit = (data) => {
-        console.log(data)
+        localStorage.setItem('title', data.title)
+        localStorage.setItem('description', data.description)
+        localStorage.setItem('price', data.price)
+        localStorage.setItem('public', data.public)
+    }
+
+    const getFromLocalStorage = (field) => {  
+        return localStorage.getItem(field)
+    }
+
+    const setIntoLocalStarage = (data) => { //TODO: Неработате так как нужно, может лучше вообще удалить!?
+        const field = data.target.id
+        localStorage.setItem(field, data.target.value)
     }
        
     return(
         <div className="add">
             <h1>Add</h1>
             <form className="form_add" onSubmit={handleSubmit(onSubmit)}>
-                <div>
+                <div className="input_field_container">
                     <div>
                         <label htmlFor="title">Title: </label>
                     </div>
-                    <input id="title" className="input_field" {...register("title", { required: true })} />
+                    <input 
+                        id="title" 
+                        className="input_field" 
+                        {...register("title", { required: true })} 
+                        defaultValue={getFromLocalStorage('title')} 
+                        onInput={setIntoLocalStarage} 
+                    />
                     {errors.title && <span className="input_error" > This field is required</span>}
                 </div>
-                <div>
+                <div className="input_field_container">
                     <div>
                         <label htmlFor="description">Description: </label>
                     </div>
-                    <input id="description" className="input_field" {...register("decription", { required: true })} />
+                    <input 
+                        id="description" 
+                        className="input_field" 
+                        {...register("description", { required: true })} 
+                        defaultValue={getFromLocalStorage('description')} 
+                        onInput={setIntoLocalStarage} 
+                    />
                     {errors.description && <span className="input_error" > This field is required</span>}
                 </div>
-                <div>
+                <div className="input_field_container">
                     <div>
                         <label htmlFor="price">Price: </label>
                     </div>
-                    <input id="price" className="input_field" {...register("price", { required: true, pattern: /\d/g })} />
+                    <input 
+                        id="price" 
+                        className="input_field" 
+                        {...register("price", { required: true, pattern: /^[0-9]+$/g })} 
+                        defaultValue={getFromLocalStorage('price')} 
+                        onInput={setIntoLocalStarage} 
+                    />
                     {errors.price && <span className="input_error"> This field is required and need only numbers</span>}
                 </div>
-                <div>
+                <div className="input_field_container">
                     <label htmlFor="public">Public</label>
-                    <input id="public" type="checkbox" {...register("public", { required: false })} />
+                    <input 
+                        id="public" 
+                        type="checkbox" 
+                        {...register("public", { required: false })} 
+                        defaultChecked={getFromLocalStorage('public') === 'true' ? true : false} 
+                    />
                 </div>
             
-                <input id="submit" type="submit" value={'Add product'}/>
+                <input id="submit" type="submit" value={'Add product'} />
             </form>
         </div>
     )
